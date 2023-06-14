f63c11f78b15dbcf7f8951b549cbb6a3783185fd
tuyendv <tuyen.duongvan268@gmail.com>
2023-06-14 13:27:28 +0700

    update

---
+++ demo/app.py
@@ -0,0 +1,88 @@
+import os
+from tqdm.auto import tqdm
+tqdm.pandas()
+from argparse import ArgumentParser
+import openai
+import gradio as gr
+import torch
+from omegaconf import OmegaConf
+from model import Cross_Model
+from importlib.machinery import SourceFileLoader
+from transformers import RobertaModel
+from transformers import AutoModel
+from transformers import AutoTokenizer
+from bm25 import BM25
+
+def init_model_and_tokenizer(config):
+    AUTH_TOKEN = "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"
+    if config.general.plm == "envibert":
+        tokenizer = SourceFileLoader(
+            "envibert.tokenizer", 
+            os.path.join(config.path.pretrained_dir,'envibert_tokenizer.py')) \
+                .load_module().RobertaTokenizer(config.path.pretrained_dir)
+        plm = RobertaModel.from_pretrained(config.path.pretrained_dir)
+    elif config.general.plm == "xlmr":
+        tokenizer = AutoTokenizer.from_pretrained(
+            'nguyenvulebinh/vi-mrc-base', cache_dir=config.path.pretrained_dir, use_auth_token=AUTH_TOKEN)
+        plm = AutoModel.from_pretrained(
+            "nguyenvulebinh/vi-mrc-base", cache_dir=config.path.pretrained_dir, use_auth_token=AUTH_TOKEN)
+    
+    model = Cross_Model(
+        max_length=config.general.max_length, 
+        batch_size=config.general.batch_size,
+        device=config.general.device,
+        tokenizer=tokenizer, model=plm)
+    
+    if os.path.exists(config.path.warm_up):
+        state_dict = torch.load(config.path.warm_up, map_location="cpu")
+        state_dict = {"module.".join(key.split("module.")[1:]):value for key, value in state_dict.items()}
+        model.load_state_dict(state_dict)
+        print(f"load model state dict from {config.path.warm_up}")
+        
+    return model, tokenizer
+
+config = OmegaConf.load("config.yaml")
+model, tokenizer = init_model_and_tokenizer(config)
+bm25_model = BM25()
+bm25_model.load("checkpoints/bm25")
+
+def infer(query):
+    bm25_result = bm25_model.search(query=query, topk=20)
+    docs = [sample[1] for sample in bm25_result]
+    
+    scores, ranks = model.ranking(query=query, texts=docs)
+
+    print("QUERY: ", query)    
+    print("MODEL_SCORES: ", scores)
+
+    top_k = [docs[i] for i in ranks]
+    results = ""
+    for index, context in enumerate(top_k):
+        print(f"###TOP_{index}: {context}")
+        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>")
+        
+        results = results + f"###TOP_{index}: {context}\n"
+                
+    return results
+
+
+if __name__ == "__main__":
+    gr.Interface(
+        fn=infer,
+        inputs=[
+            gr.components.Textbox(
+                lines=1,
+                label="Query",
+                placeholder="giới thiệu về mobifone.",
+            )
+        ], 
+        outputs=[
+            gr.components.Textbox(
+                minimum=1,
+                maximum=2000,
+                label="Docs",
+            )
+        ],
+        title="ChatBot Demo",
+        description="Duong Van Tuyen",
+    ).launch(share=True)
---
+++ demo/model.py
@@ -0,0 +1,80 @@
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+import pandas as pd
+
+import torch
+import torch.nn as nn
+from utils import norm_text
+
+from dataset import (
+    Infer_Pairwise_Dataset
+)
+
+class Cross_Model(nn.Module):
+    def __init__(self, model, tokenizer, max_length=384, droprate=0.2, batch_size=16, device="cpu"):
+        super(Cross_Model, self).__init__()
+        self.max_length = max_length
+        self.batch_size = batch_size
+        
+        self.model = model
+        self.device = device
+        total_layer = len(self.model.encoder.layer)
+        num_freeze_layer = int(2*total_layer/3)
+        print(f"freezing {num_freeze_layer} layer")
+        modules = [self.model.embeddings, self.model.encoder.layer[:num_freeze_layer]]
+        
+        for module in modules:
+            for param in module.parameters():
+                param.requires_grad = False
+                
+        self.tokenizer = tokenizer
+        
+        self.dropout = nn.Dropout(droprate)
+        self.fc = nn.Linear(768, 1)
+        self.cre = torch.nn.CrossEntropyLoss()
+
+    def forward(self, ids, masks, labels=None, context_masks=None):
+        out = self.model(input_ids=ids,
+                         attention_mask=masks)
+        out = out.last_hidden_state[:, 0]
+        embedding = self.dropout(out)
+        logits = self.fc(embedding)
+        if labels is not None:
+            logits = logits.reshape(labels.size(0), labels.size(1))
+            return logits, self.loss(labels=labels, logits=logits, context_masks=context_masks)
+        
+        return logits
+    
+    def loss(self, labels, logits, context_masks):
+        exp = torch.exp(logits)
+        exp = torch.masked_fill(input=exp, mask=~context_masks, value=0)
+        loss = -torch.log(torch.sum(torch.mul(exp, labels)) / torch.sum(exp))
+        
+        return loss
+    
+    @torch.no_grad()
+    def ranking(self, query, texts):
+        tmp = pd.DataFrame()
+        tmp["text"] = [norm_text(x) for x in texts]
+        tmp["query"] = norm_text(query)
+        
+        valid_dataset = Infer_Pairwise_Dataset(
+            tmp, self.tokenizer, self.max_length)
+        
+        valid_loader = DataLoader(
+            valid_dataset, batch_size=self.batch_size, collate_fn=valid_dataset.infer_collate_fn,
+            num_workers=0, shuffle=False, pin_memory=True)
+        preds = []
+        with torch.no_grad():
+            bar = tqdm(enumerate(valid_loader))
+            for step, data in bar:
+                ids = data["ids"].to(self.device)
+                masks = data["masks"].to(self.device)
+                preds.append(torch.sigmoid(self(ids, masks)).view(-1))
+            preds = torch.concat(preds)
+        
+        scores = preds.cpu()
+        ranks = scores.argsort(descending=True)
+        print("model score: ", preds)
+        print("rank: ", ranks)
+        return scores, ranks
