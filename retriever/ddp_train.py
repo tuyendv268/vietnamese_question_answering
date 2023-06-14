cb82e736ca4fd52bf29da16911bef278e174bb11
tuyendv <tuyen.duongvan268@gmail.com>
2023-06-14 20:01:24 +0700

    update

--- retriever/ddp_train.py
+++ retriever/ddp_train.py
@@ -223,6 +223,7 @@ def train(config):
                 logits = [pairwise_cosine_similarity(x, y) for x, y in zip(query_embeddings, context_embeddings)]            
                 logits = torch.cat(logits, dim=0)
                 
+                loss = model.loss(labels, logits, context_masks)  
                 loss /= config.general.accumulation_steps
             loss.backward()
 
